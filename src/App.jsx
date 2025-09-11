cb7547ccead760427563f0c907aa5248a48433de
Wahid Patel <patel.wahid2005@gmail.com>
2025-09-11 21:50:30 +0530

    changed admin password

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
 
 
   const handleLogin = (email, password) => {
-    if (email == 'admin@me.com' && password == '123') {
+    if (email == 'admin@me.com' && password == 'Wahid@p12') {
       setUser('admin')
       localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
     } else if (userData) {
